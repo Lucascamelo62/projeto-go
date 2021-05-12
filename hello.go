0fda178976c9f85488ffed77636c241d11cb2159
camelo <lucascamelo62@gmail.com>
2021-05-11 23:22:54 -0300

    Segunda aula

--- hello.go
+++ hello.go
@@ -3,6 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello Word")
-	fmt.Println("Lucas Silva Camelo")
+	nome := "Lucas Camelo"
+	versao := 1.1
+
+	fmt.Println("Olá", nome,)
+	fmt.Println("Versão do Sistema: ", versao)
+
+	fmt.Println("1- Monitoramento")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("0- Sair")
+	fmt.Println("Digite um dos comandos acima:")
+
+	var comando int
+	fmt.Scan(&comando)
+	fmt.Println("A opção digitida foi: ", comando)
 }
